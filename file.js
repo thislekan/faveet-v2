b9682704b295df9c85542d14cf7d4d7aab3a333d
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2018-02-06 03:18:00 +0100

    Client Story Prototype

--- file.js
+++ file.js
@@ -2,42 +2,76 @@ const navBtn = document.getElementById('nav-btn');
 const mobileNav = document.querySelector('.nav__inside');
 const serviceNavLinks = document.querySelectorAll('._container_tab');
 const clientDetails = document.querySelectorAll('.clients__story');
+const navBar = document.querySelector('.nav-cont');
+const foo = document.getElementById('foo');
+const clientStory = document.querySelectorAll('.clients__story');
+
 
 if (window.innerWidth <= 499) {
     mobileNav.style.left = '100%';
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > window.innerHeight) {
+            navBtn.style.color = 'initial';
+        } else {
+            navBtn.style.color = 'inherit';
+        }
+    })
 }
 navBtn.addEventListener('click', () => {
     if (mobileNav.style.left === '100%') {
         mobileNav.style.left = '70%';
+        navBtn.style.color = 'inherit';
     } else {
         mobileNav.style.left = '100%';
+        navBtn.style.color = 'initial';
     }
 });
 
-// clientDetails.forEach(details => {
-//     details.style.display = 'none';
-//     details.classList.add('inactive');
-// });
-for (let i = 0; i < clientDetails.length; i++) {
-    const element = clientDetails[i];
-    element.classList.add('inactive');
-    clientDetails[0].classList.remove('inactive');
-}
-
-for (let i = 0; i < serviceNavLinks.length; i++) {
-    const element = serviceNavLinks[i];
-    element.addEventListener('click', () => {
-        clientDetails[i].classList.remove('inactive');
-        clientDetails[i].classList.add('active');
-        // if (clientDetails[i].classList.contains('active')) {
-        //     setTimeout(() => {
-        //         clientDetails[i].style.display = 'block'
-        //     }, 800);
-        // } else {
-        //     clientDetails[i].style.display = 'none';
-        // }
+if (window.innerWidth >= 500) {
+    window.addEventListener('scroll', e => {
+        if (scrollY >= navBar.offsetTop) {
+            navBar.classList.add('fixed-nav');
+            if (scrollY <= window.innerHeight) {
+                navBar.classList.remove('fixed-nav');
+            }
+        }
+        window.addEventListener('keydown', e => {
+            if (navBar.classList.contains('fixed-nav')) {
+                if (e.keyCode === 38) {
+                    navBar.classList.remove('navbar-animate');
+                }
+                if (e.keyCode === 40) {
+                    navBar.classList.add('navbar-animate');
+                }
+            }
+        });
     });
 }
+// for (let i = 0; i < serviceNavLinks.length; i++) {
+//     const element = serviceNavLinks[i];
+//     element.addEventListener('click', e => {
+//         if (e) {
+//             console.log(e.target);
+//         }
+//     })
+// }
+
+foo.addEventListener('click', () => {
+    console.log(clientStory[1]);
+    let yuu = clientStory[1];
+    for (let i = 0; i < clientStory.length; i++) {
+        const element = clientStory[i];
+        if (!element.classList.contains('inactive')) {
+            element.classList.add('inactive');
+        }
+    }
+    yuu.classList.remove('inactive');
+    yuu.classList.add('active');
+    setTimeout(() => {
+        // alert('yuuup');
+        yuu.classList.add('story-animate');
+    }, 500);
+});
 
 function debounce(cb, wait = 20, immediate = true) {
     var timeout;
